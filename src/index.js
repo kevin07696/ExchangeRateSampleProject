d44d415d1188495cea10ed47ba19a706ba5f8b17
Kevin Lam <kevinlam07696@gmail.com>
2021-07-28 02:10:15 -0400

    Enabled search currency

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates($currency: String!) {
+    rates(currency: $currency) {
+      currency
+      rate
+    }
+  }
+`;
+
+
+function ExchangeRates({ currency }) {
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES, {
+    variables: { currency },
+    pollInterval: 500,
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error}</p>;
+  if (data.rates == null) {
+    return <p>No currencys match search</p>;
+  }
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+
+function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  return (
+    <div>
+      <h2>Search Currency to See its Exchange Rates</h2>
+      <input
+        type="text"
+        placeholder="Search defaults to USD"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ExchangeRates currency={searchTerm || 'USD'}/>
+    </div>
+  );
+}
+
+
+render(
+
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
