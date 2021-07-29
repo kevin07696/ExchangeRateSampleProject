6b4b79dab293970d11d4adc8fff068b09ae33e1c
Kevin Lam <kevinlam07696@gmail.com>
2021-07-29 02:31:10 -0400

    Enabled Search with Suggestions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  gql,
 } from "@apollo/client";
+import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache()
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
 });
 
 const EXCHANGE_RATES = gql`
+  query GetRates {
+    rates(currency: "USD") {
+      currency
+    }
+  }
+`;
+
+const SELECT_CURRENCY = gql`
   query GetExchangeRates($currency: String!) {
     rates(currency: $currency) {
       currency
@@ -22,10 +32,8 @@ const EXCHANGE_RATES = gql`
   }
 `;
 
-
 function ExchangeRates({ currency }) {
-
-  const { loading, error, data } = useQuery(EXCHANGE_RATES, {
+  const { loading, error, data } = useQuery(SELECT_CURRENCY, {
     variables: { currency },
     pollInterval: 500,
   });
@@ -33,7 +41,7 @@ function ExchangeRates({ currency }) {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error}</p>;
   if (data.rates == null) {
-    return <p>No currencys match search</p>;
+    return <p>No currency match search</p>;
   }
   return data.rates.map(({ currency, rate }) => (
     <div key={currency}>
@@ -44,31 +52,49 @@ function ExchangeRates({ currency }) {
   ));
 }
 
-
 function App() {
-  const [searchTerm, setSearchTerm] = React.useState("");
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-  };
+  const [searchTerm, setSearchTerm] = React.useState("");    
+  const { error, loading, data } = useQuery(EXCHANGE_RATES);
+  const methods = useForm();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error : {error}</p>;
+
+  if (data == null) {
+    return <p>No Data</p>;
+  }
+  const options = [];
+  data.rates.forEach((rate) => {
+    options.push({ value: rate.currency, label: rate.currency });
+  });
+  console.log(options);
+
   return (
     <div>
       <h2>Search Crypto to See its Exchange Rates</h2>
-      <input
-        type="text"
-        placeholder="Search defaults to USD"
-        value={searchTerm}
-        onChange={handleChange}
+      <Controller
+        control={methods.control}
+        defaultValue='USD'
+        name="Search Crypto"
+        render={({ value, ref }) => (
+          <Select
+            inputRef={ref}
+            options={options}
+            value={options.find((c) => c.value === value)}
+            onChange={(val) => setSearchTerm(val.value)}
+          />
+        )}
       />
-      <ExchangeRates currency={searchTerm || 'USD'}/>
+
+      <ExchangeRates currency={searchTerm || "USD"} />
     </div>
   );
 }
 
-
 render(
-
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
